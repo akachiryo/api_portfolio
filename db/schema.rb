971285653a1757dcb222e57cbb076d5c1b9d9cc6
akachiryo <rirugameri@icloud.com>
2022-05-24 00:06:07 +0900

    user index　レスポンス　成功

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_05_23_123811) do
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.text "introduction"
+    t.string "avatar"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
