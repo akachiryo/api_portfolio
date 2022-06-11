bad119d2ab22c00390990ee9a56d464e78271153
akachiryo <rirugameri@icloud.com>
2022-06-12 00:40:27 +0900

    update 準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   has_secure_password
   has_one_attached :avatar
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
+   def avatar_url
+    # 紐づいている画像のURLを取得する
+    avatar.attached? ? url_for(avatar) : nil
+  end
 end
