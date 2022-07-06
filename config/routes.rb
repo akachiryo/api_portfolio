03bc1d55afc9f7d1b4c6bb75492d09abad080a4f
akachiryo <rirugameri@icloud.com>
2022-07-06 23:41:49 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :users, only: %i[index create update]
+    resources :users, only: %i[index create update], format: "json"
     resources :skills, only: %i[show create update destroy], format: "json"
     resources :skill_types, only: %i[show], format: "json"
     resource :session, only: %i[create destroy], format: "json"
